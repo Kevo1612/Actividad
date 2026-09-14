9a48350c53e95b1572426221be33bbc20bb4cc35
Kevo1612 <k_vin_k@hotmail.com>
2026-09-14 16:12:28 -0300

    Clean

--- demo/src/main/java/com/example/demo/repository/InteresRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Interes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InteresRepository extends JpaRepository<Interes, Long> {
-}
--- demo/src/main/java/com/example/demo/repository/TransaccionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Transaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
-}
